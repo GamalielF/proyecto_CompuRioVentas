c7d4a7ef978474ba6e2ab194ef20a746fc292912
pc <pc@WINDOWS-93NJRPO>
2020-07-14 16:21:49 -0500

    primera version

---
+++ Appcompurioventas/src/main/java/net/osgg/Appcompurioventas/AppcompurioventasApplication.java
@@ -0,0 +1,13 @@
+package net.osgg.Appcompurioventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcompurioventasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcompurioventasApplication.class, args);
+	}
+
+}
---
+++ Appcompurioventas/src/test/java/net/osgg/Appcompurioventas/AppcompurioventasApplicationTests.java
@@ -0,0 +1,13 @@
+package net.osgg.Appcompurioventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcompurioventasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
